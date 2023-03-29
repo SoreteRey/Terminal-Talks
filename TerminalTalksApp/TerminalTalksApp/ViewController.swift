a5d96452442e634b505a5fb3c9558ae7017ad2ef
sguiscardo <sdguiscardo@gmail.com>
2023-03-29 10:35:23 -0500

    Adds initial project file

---
+++ TerminalTalksApp/TerminalTalksApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TerminalTalksApp
+//
+//  Created by Sebastian Guiscardo on 3/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
